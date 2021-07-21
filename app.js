da1acd41475ab14eeb8f96d17ff25fcfa157ac95
Tamilselvan <tamilselvan.guru@gmail.com>
2021-07-21 19:48:54 +0530

    added middlewares and routes

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.listen(port, () => console.log(`Example app listening on port ` + port))
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port port!`))
+//Middlewares
+app.use('/posts', (req, res , next) => {
+    console.log('Thid is middleware logging')
+    next()
+});
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('we are on HOME')
+});
+
+app.get('/posts', (req,res) => {
+    res.send('we are on posts')
+});
