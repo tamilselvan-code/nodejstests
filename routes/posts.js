892bab8bd61b76e7d6d196fa5ce28888c8dd7112
Tamilselvan <tamilselvan.guru@gmail.com>
2021-07-21 21:48:06 +0530

    added routes and schema for db

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,19 @@
 const express = require('express');
+
 const router = express.Router();
 
-router.get('/posts', (req,res) => {
+const Post = require('./models/Post');
+
+router.get('/', (req,res) => {
     res.send('we are on posts')
+});
+
+router.get('/specific', (req,res) => {
+    res.send('we are on specific')
+});
+
+router.post('/', (req, res) => {
+    
 });
 
 module.exports = router;
